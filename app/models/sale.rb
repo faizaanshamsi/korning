2eb1e520ceed417dececb640b4e25908824083c8
landonmarder <landon.marder@gmail.com>
2013-12-14 16:32:05 -0500

    show sales for last 13 months

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,4 +3,12 @@ class Sale < ActiveRecord::Base
   belongs_to :customer_and_account_no
   belongs_to :product_name
   belongs_to :invoice_frequency
+
+  def self.month_sales(num)
+    Sale.where("sale_date >= ?", num.months.ago)
+  end
+
+  def price_per_unit
+    (sale_amount / units_sold).round(2)
+  end
 end
